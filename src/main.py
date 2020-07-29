37d2762e16761918322798fede05ea9f82939280
Fabio Azevedo <emaildofabioeduardo@gmail.com>
2020-07-29 19:48:30 -0300

    add main.py

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import uvicorn
+from typing import List
+from functools import lru_cache
+from fastapi import FastAPI
+from models import JsonIn, JsonOut, JsonAppdynamicsIn, JsonAppdynamicsOut
+from zbxsender import Sender
+import conf
+
+
+app = FastAPI()
+
+
+@lru_cache()
+def get_settings():
+    return conf.DevConfig()
+
+
+@app.post("/alert/", response_model=JsonOut, status_code=201)
+async def alert_value(host: JsonIn):
+    result = JsonOut(**host.dict(), received='Faild')
+    return result
+
+
+if __name__ == "__main__":
+    conf = get_settings()
+    uvicorn.run(app, host="0.0.0.0", port=conf.APP_PORT)
